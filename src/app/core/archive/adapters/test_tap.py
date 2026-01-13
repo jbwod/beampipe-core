385c4809672f29cf716daffce1bfc02dfa6472cf
Jack Blackwood <cybercow.now@gmail.com>
2026-01-13 21:30:39 +1100

    Debugging the Vizier TAP and replicating existing logic.

--- src/app/core/archive/adapters/test_tap.py
+++ src/app/core/archive/adapters/test_tap.py
@@ -6,7 +6,7 @@ from astroquery.utils.tap.core import TapPlus
 CASDA_TAP_URL = "https://casda.csiro.au/casda_vo_tools/tap"
 VIZIER_TAP_URL = "http://tapvizier.cds.unistra.fr/TAPVizieR/tap"
 
-source = "HIPASSJ1303+07"
+source = "HIPASSJ1318-21"
 
 casdatap = TapPlus(url=CASDA_TAP_URL, verbose=False)
 query = f"SELECT * FROM ivoa.obscore WHERE filename LIKE '{source}%'"
@@ -22,10 +22,21 @@ results_eval = job_eval.get_results()
 print(f"\nEvaluation files for SBID {sbid}: {len(results_eval)}")
 print(results_eval)
 
-source_name = source.replace("HIPASS", "").strip()
+if "HIPASS" in source:
+    source_name = source[source.index("HIPASS") + len("HIPASS") :].strip()
+else:
+    source_name = source.strip()
+
+HIPASS_QUERY_RA_DEC_VSYS = (
+    'SELECT RAJ2000, DEJ2000, VSys FROM "J/AJ/128/16/table2" WHERE '
+    "HIPASS LIKE '$filename'"
+)
+query_coords = HIPASS_QUERY_RA_DEC_VSYS.replace("$filename", source_name)
+print(f"\nRA DEC VSYS Query: {query_coords}")
+
 viziertap = TapPlus(url=VIZIER_TAP_URL, verbose=False)
-query_coords = f'SELECT RAJ2000, DEJ2000, VSys FROM "J/AJ/128/16/table2" WHERE HIPASS LIKE \'{source_name}\''
 job_coords = viziertap.launch_job_async(query_coords)
 results_coords = job_coords.get_results()
+print(f"Query result: {results_coords}")
 print(f"\nCoordinates: {len(results_coords)}")
 print(results_coords)
