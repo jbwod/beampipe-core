283b40f9c8e4054dd1e13f96ff0f46f9b2d08de3
Jack Blackwood <cybercow.now@gmail.com>
2026-01-13 21:29:48 +1100

    Cleanup

--- src/app/core/archive/adapters/test_tap.py
+++ src/app/core/archive/adapters/test_tap.py
@@ -2,7 +2,9 @@ from astropy.table import Table
 from astroquery.utils.tap.core import TapPlus
 # https://astroquery.readthedocs.io/en/latest/casda/casda.html
 # https://github.com/ICRAR/wallaby-hires/blob/main/wallaby_hires/funcs.py
+
 CASDA_TAP_URL = "https://casda.csiro.au/casda_vo_tools/tap"
+VIZIER_TAP_URL = "http://tapvizier.cds.unistra.fr/TAPVizieR/tap"
 
 source = "HIPASSJ1303+07"
 
@@ -20,3 +22,10 @@ results_eval = job_eval.get_results()
 print(f"\nEvaluation files for SBID {sbid}: {len(results_eval)}")
 print(results_eval)
 
+source_name = source.replace("HIPASS", "").strip()
+viziertap = TapPlus(url=VIZIER_TAP_URL, verbose=False)
+query_coords = f'SELECT RAJ2000, DEJ2000, VSys FROM "J/AJ/128/16/table2" WHERE HIPASS LIKE \'{source_name}\''
+job_coords = viziertap.launch_job_async(query_coords)
+results_coords = job_coords.get_results()
+print(f"\nCoordinates: {len(results_coords)}")
+print(results_coords)
