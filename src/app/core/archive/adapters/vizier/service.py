14b0e920b022fcca88b0e354f5c91cff12644496
Jack Blackwood <cybercow.now@gmail.com>
2026-02-13 16:01:46 +1100

    Vizier Adapter

--- src/app/core/archive/adapters/vizier/service.py
+++ src/app/core/archive/adapters/vizier/service.py
@@ -12,6 +12,27 @@ VIZIER_TAP_URL = "http://tapvizier.cds.unistra.fr/TAPVizieR/tap"
 VIZIER_TAP_HEALTH_URL = "https://tapvizier.cds.unistra.fr/TAPVizieR/tap/async"
 
 
+class VizierDiscoverAdapter:
+    """Vizier adapter implementing the DiscoverAdapter protocol for registry and health checks."""
+
+    def __init__(
+        self, tap_url: str = VIZIER_TAP_URL, health_url: str = VIZIER_TAP_HEALTH_URL
+    ) -> None:
+        self._tap_url = tap_url
+        self._health_url = health_url
+
+    @property
+    def health_url(self) -> str:
+        return self._health_url
+
+    @property
+    def tap_url(self) -> str:
+        return self._tap_url
+
+    def query(self, query_str: str, tap_url: Optional[str] = None) -> Table:
+        return query(query_str, tap_url=tap_url or self._tap_url)
+
+
 def query(query: str, tap_url: Optional[str] = None) -> Table:
     """Run a TAP query against Vizier (or an overridden TAP URL)."""
     tap_endpoint = tap_url or VIZIER_TAP_URL
