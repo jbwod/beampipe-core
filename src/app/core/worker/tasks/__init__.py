de37cc9f0229cda111a1ffadb21fe3d6937eeab3
Jack Blackwood <cybercow.now@gmail.com>
2026-02-16 11:34:18 +1100

    Seperating the Worker functions

---
+++ src/app/core/worker/tasks/__init__.py
@@ -0,0 +1,18 @@
+from .discovery import discover_batch
+from .lifecycle import shutdown, startup
+from .scheduler import (
+    discover_schedule_task,
+    enqueue_timer_task,
+    sample_background_task,
+    timer_task,
+)
+
+__all__ = [
+    "discover_batch",
+    "discover_schedule_task",
+    "enqueue_timer_task",
+    "sample_background_task",
+    "shutdown",
+    "startup",
+    "timer_task",
+]
