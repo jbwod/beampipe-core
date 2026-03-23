bc020320d75bc9e05b8b81be4a21f562f1c892ee
Jack Blackwood <cybercow.now@gmail.com>
2026-03-24 00:11:54 +1100

    Allow for an empty execution profile

--- src/app/models/daliuge.py
+++ src/app/models/daliuge.py
@@ -19,9 +19,9 @@ class DaliugeExecutionProfile(Base):
         UUID(as_uuid=True), primary_key=True, default_factory=uuid7, unique=True, init=False
     )
     name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True, index=True)
-    tm_url: Mapped[str] = mapped_column(String(255), nullable=False)
-    dim_host_for_tm: Mapped[str] = mapped_column(String(100), nullable=False)
-    dim_port_for_tm: Mapped[int] = mapped_column(nullable=False)
+    tm_url: Mapped[str | None] = mapped_column(String(255), nullable=True, default=None)
+    dim_host_for_tm: Mapped[str | None] = mapped_column(String(100), nullable=True, default=None)
+    dim_port_for_tm: Mapped[int | None] = mapped_column(nullable=True, default=None)
     deploy_host: Mapped[str | None] = mapped_column(String(100), nullable=True, default=None)
     deploy_port: Mapped[int | None] = mapped_column(nullable=True, default=None)
     deployment_backend: Mapped[str] = mapped_column(
