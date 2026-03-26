49c9ad0eab10d9c35d928fd618da3d547f0f3d00
Jack Blackwood <cybercow.now@gmail.com>
2026-03-26 23:02:35 +1100

    Adding execute workflow to ARQ Worker

--- src/app/core/worker/functions.py
+++ src/app/core/worker/functions.py
@@ -6,10 +6,12 @@ from .tasks import (
     discover_batch,
     discover_schedule_task,
     enqueue_timer_task,
+    execute_run_job,
     sample_background_task,
     shutdown,
     startup,
     timer_task,
+    workflow_run_schedule_task,
 )
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
@@ -19,9 +21,11 @@ asyncio.set_event_loop(_loop)
 __all__ = [
     "discover_batch",
     "discover_schedule_task",
+    "execute_run_job",
     "enqueue_timer_task",
     "sample_background_task",
     "shutdown",
     "startup",
     "timer_task",
+    "workflow_run_schedule_task",
 ]
